21a1e63d02fe50ac94483fb09813dac963a12656
bplotka <bwplotka@gmail.com>
2014-11-05 01:48:11 +0100

    Added hb functionality.
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- src/cloudNote/DbHelper.java
+++ src/cloudNote/DbHelper.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.dialect.DB2390Dialect;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
@@ -53,20 +54,52 @@ public class DbHelper {
         return (UserEntity) criteria.uniqueResult();
     }
 
+    public static TokenEntity getTokenByUser(Session session, UserEntity user, Boolean check_last_hb)  throws Exception {
+
+        Criteria criteria = session.createCriteria(TokenEntity.class);
+        criteria.add(Restrictions.eq("userId", user.getId()));
+        return (TokenEntity) criteria.uniqueResult();
+    }
+
     public static void createUserSession(Session session, UserEntity user, String token, Boolean uniq)  throws Exception {
+        java.util.Date date = new java.util.Date();
         if (uniq) {
-            //TODO: check if uniq
+
+            if (DbHelper.getTokenByUser(session, user, true) != null){
+                throw new Exception("There is a session for that user! Cannot login");
+            }
         }
         session.beginTransaction();
         TokenEntity tokenEntity = new TokenEntity();
         tokenEntity.setUserId(user.getId());
-        java.util.Date date = new java.util.Date();
+
         tokenEntity.setCreateTime(new Timestamp(date.getTime()));
         tokenEntity.setUpdateTime(Timestamp.valueOf("2015-10-10 10:10:10"));
         tokenEntity.setToken(token);
         session.save(tokenEntity);
 
         session.getTransaction().commit();
+    }
+
+    public static Boolean isTokenValid(Session session, String mail, String token, Boolean update_session_time) throws Exception{
+        java.util.Date date = new java.util.Date();
+        UserEntity user = DbHelper.getUserByLogin(session, mail);
+        if (user == null){
+            throw new Exception("There is no such user");
+        }
+        TokenEntity token_from_db = DbHelper.getTokenByUser(session, user, true);
+        if (token_from_db != null){
+            if (token_from_db.getToken().equals(token)){
+                if (update_session_time){
+                    session.beginTransaction();
+                    token_from_db.setUpdateTime(new Timestamp(date.getTime()));
+                    session.update(token_from_db);
+                    session.getTransaction().commit();
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
 //    public static void createUserSession(Session session, UserEntity user, String token)  throws Exception {
--- src/cloudNote/HeartBeatServlet.java
+++ src/cloudNote/HeartBeatServlet.java
@@ -1,11 +1,14 @@
 package cloudNote;
 
+import org.hibernate.Session;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,12 +17,13 @@ import java.util.Map;
  */
 public class HeartBeatServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Call: heartbeat from user");
+        System.out.println("Hb call: heartbeat");
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF8");
         PrintWriter out = response.getWriter();
         ApiHelper.Status status = ApiHelper.Status.OK;
         Map<String, String> return_fields = new HashMap<String, String>();
+        Session session = null;
         try {
             String mail = request.getParameter("login");
             String token = request.getParameter("token");
@@ -27,12 +31,11 @@ public class HeartBeatServlet extends HttpServlet {
             if ((mail == null)||(token == null)){
                 throw new Exception("No token or login specified in request");
             }
-            else {
-            /* TODO: DB Check token */
-            }
 
-            if (auth) {
-                /* TODO: Update last hb in user session DB*/
+            System.out.println("Hb call:: heartbeat from user " + mail);
+            session = DbHelper.getCreatedSession();
+
+            if (DbHelper.isTokenValid(session, mail, token, true)) {
                 return_fields.put("msg", "Updated session");
                 out.println(ApiHelper.returnJson(status, return_fields));
             }else{
